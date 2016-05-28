469132583980f11b17e8e4185132b6d9c2e5ed99
akr5079 <aharon.robinson@outlook.com>
2016-05-28 13:21:06 -0700

    Pushing VideoPlayer.cs

---
+++ 360VideoPlayer/VideoPlayer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class VideoPlayer : MonoBehaviour {
+    public MovieTexture movTexture;
+
+    // Use this for initialization
+    void Start () {
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
